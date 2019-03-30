dd645a5d192d5b0bda481570abb4db7f1890b546
Laurence <laurencehall1@outlook.com>
2019-03-29 20:42:56 -0400

    fixed animation bugs

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 $(document).ready(function() {
   $("main").css("opacity", 0);
-  $("#info").hide();
+  $("#info").css("opacity", 0);
 
   setTimeout(() => {
-    $("#info").show();
+    $("#info").css("opacity", 1);
     $("#info").addClass("animated bounceInUp");
   }, 1000);
 
   setTimeout(() => {
-    $("main").show();
+    $("main").css("opacity", 1);
     $("main").addClass("animated fadeInUp");
   }, 2600);
-  $("h1").addClass("animated rotateIn");
+  $("h1").addClass("animated heartBeat");
+
+  setTimeout(() => {
+    $("h1").removeClass("animated heartBeat");
+  }, 1500);
+
+  $("h1").css("pointer-events", "none");
+
+  setTimeout(() => {
+    $("h1").css("pointer-events", "auto");
+  }, 3000);
 
   $("h1").hover(function() {
-    $("h1").removeClass("animated rotateIn");
-
     $(this).toggleClass("animated shake");
   });
 
   $("a").hover(function() {
-    $(this).toggleClass("animated pulse");
+    $(this).toggleClass("animated jello");
   });
 });
